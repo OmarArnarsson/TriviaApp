338791b9a29649e3e52f89fab469a3e770a007c6
OmarArnarsson <otha3@hi.is>
2021-08-26 20:50:12 +0000

    bæta við newline í endann

--- data.js
+++ data.js
@@ -6,15 +6,14 @@ const trivia = [
     { category: 'Entertainment: Cartoon & Animations', type:'multiple', difficulty:'easy', question:'What is lost in Hawaiian and is also the name of a little girl in a 2002 film which features a alien named "Stitch"?', correct_answer:'Lilo'},
     { category: 'Entertainment: Cartoon & Animations', type:'multiple', difficulty:'medium', question:'What is the name of the Alligator that The Simpsons "killed" in the episode "Kill The Alligator and Run"?', correct_answer:'Captain Jack'},
     { category: 'Entertainment: Cartoon & Animations', type:'multiple', difficulty:'medium', question:'What is the name of Ruby Rose\'s weapon from RWBY?', correct_answer:'Crescent Rose'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'medium', question:'The song "Twin Size Mattress" was written by which band?', correct_answer:'The Front Bottoms'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'medium', question:'Which singer was featured in Swedish producer Avicii\'s song "Wake Me Up"?', correct_answer:'Aloe Blacc'},
-    { category:'Entertainment: Music', type:'boolean', difficulty:'medium', question:'Scottish producer Calvin Harris is from the town of Dumfries, Scotland.', correct_answer:'True'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'medium', question:'What year was Red Hot Chill Pepper\'s album "Californication" released?', correct_answer:'1999'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'easy', question:'What album did The Lumineers release in 2016?', correct_answer:'Cleopatra'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'easy', question:'"The Singing Cowboy" Gene Autry is credited with the first recording for all but which classic Christmas jingle?', correct_answer:'White Christmas'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'medium', question:'"Make You Feel My Love" was originally written and performed by which singer-songwriter?', correct_answer:'Bob Dylan'},
-    { category:'Entertainment: Music', type:'multiple', difficulty:'medium', question:'In what year was Tchaikovsky\'s 1812 Overture composed?', correct_answer:'1880'}
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'medium', question:'The song "Twin Size Mattress" was written by which band?', correct_answer:'The Front Bottoms'},
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'medium', question:'Which singer was featured in Swedish producer Avicii\'s song "Wake Me Up"?', correct_answer:'Aloe Blacc'},
+    { category: 'Entertainment: Music', type:'boolean', difficulty:'medium', question:'Scottish producer Calvin Harris is from the town of Dumfries, Scotland.', correct_answer:'True'},
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'medium', question:'What year was Red Hot Chill Pepper\'s album "Californication" released?', correct_answer:'1999'},
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'easy', question:'What album did The Lumineers release in 2016?', correct_answer:'Cleopatra'},
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'easy', question:'"The Singing Cowboy" Gene Autry is credited with the first recording for all but which classic Christmas jingle?', correct_answer:'White Christmas'},
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'medium', question:'"Make You Feel My Love" was originally written and performed by which singer-songwriter?', correct_answer:'Bob Dylan'},
+    { category: 'Entertainment: Music', type:'multiple', difficulty:'medium', question:'In what year was Tchaikovsky\'s 1812 Overture composed?', correct_answer:'1880'}
 ]
 
 module.exports = { trivia };
-
