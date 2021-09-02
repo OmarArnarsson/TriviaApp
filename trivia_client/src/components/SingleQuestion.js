3866ad0009c514cb824a2411c8bb01f3e9dbf361
OmarArnarsson <otha3@hi.is>
2021-09-02 16:03:38 +0000

    Tók út tilgangslausar skrár og lagadi soft villur

--- trivia_client/src/components/SingleQuestion.js
+++ trivia_client/src/components/SingleQuestion.js
@@ -6,13 +6,13 @@ import '../CSS/SingleQuestion.css'
 export default function SingleQuestion({ question: { category, question, correct_answer } }) {
 
     return (
-        <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
                     <h4>{category}</h4>
                     <p>{question}</p>
                 </div>
-                <div class="flip-card-back">
+                <div className="flip-card-back">
                     <h4>Answer</h4>
                     <p>{correct_answer}</p>
                 </div>
